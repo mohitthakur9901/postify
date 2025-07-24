8ede9c8e19225290895e036066758c62ce7b674c
mohit <mohitthakur9901@gmail.com>
2025-07-24 18:52:06 +0530

    update backend

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -4,7 +4,7 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-const fileFilter = (req : any, file : any , cb : any) => {
+const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,15 +1,14 @@
 import express from "express";
-
-import { protectRoute } from "../middleware/auth.middleware";
-import upload from "../middleware/upload.middleware";
 import {
-  getPosts,
   createPost,
   deletePost,
   getPost,
+  getPosts,
   getUserPosts,
   likePost,
-} from "../controller/post.controller.js";
+} from "../controllers/post.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
